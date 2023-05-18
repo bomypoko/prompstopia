c1720f90d95da45acdd94feb71bcad83a2f43b7d
Ratchasri Kidking <orc_hero@hotmail.com>
2023-05-18 15:36:19 +0700

    Create Mobile View

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,8 +5,42 @@ import Image from 'next/image' // optimize images when loading
 import { useState , useEffect } from 'react' // render from Client side ( Use client)
 import { signIn , signOut , useSession , getProviders } from 'next-auth/react' // for user to  login and logout 
 const Nav = () => {
-  return (
-    <div>Nav</div>
+
+    const isUserLoggedIn = true
+
+    return (
+
+        <nav className='w-full flex-between mb-16 pt-3 '>
+
+
+            <Link href="/" className='flex gap-2 flex-center'> 
+                <Image 
+                    src="/assets/images/logo.svg" 
+                    width={30} height={300}
+                    alt="Promptopia Logo"
+                    className='object-contain'/> 
+            </Link>
+            <p className='logo_text'> Promptopia</p>
+
+
+            {/* Mobile Navigation  */}
+            <div className='sm:flex hidden'>
+                {isUserLoggedIn ? (
+                
+                        <div className='flex  gap-3 md:gap-5'>
+                            <Link href="/create-prompt" className='black_btn'>
+                                Create Post
+                            </Link>
+                        </div>
+                    
+                ) : (
+                    <>
+
+                    </>
+                )}
+            </div>
+
+        </nav>
   )
 }
 
